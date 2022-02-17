e4826325b7752b63b9771a1b43e36e140d4db86b
Michal Szafranski <szafranski@gmail.com>
2022-02-17 18:16:13 +0000

    fixing failure exists and static 0 descriptior

--- player/terminput.c
+++ player/terminput.c
@@ -7,12 +7,8 @@
 struct termios old, new;
 int fd = 0;
 int oldflags = 0;
-void kbd_error(char *text) {
-  perror(text);
-  exit(EXIT_FAILURE);
-}
 void term_setup() {
-	if (tcgetattr(fd, &old) == -1) kbd_error("tcgetattr");
+	if (tcgetattr(fd, &old) == -1) perror("tcgetattr");
   new = old;
 //	new.c_lflag &= ~((tcflag_t)(ICANON | ECHO | ISIG));
 	new.c_lflag &= ~((tcflag_t)(ICANON | ECHO));
@@ -20,16 +16,16 @@ void term_setup() {
 //	new.c_iflag     = 0;
 	new.c_cc[VMIN]  = 0;
 	new.c_cc[VTIME] = 0; /* 0.1 sec intercharacter timeout */
-	if (tcsetattr(fd, TCSAFLUSH, &new) == -1) kbd_error("tcsetattr");
-  oldflags = fcntl(0, F_GETFL, 0);
+	if (tcsetattr(fd, TCSAFLUSH, &new) == -1) perror("tcsetattr");
+  oldflags = fcntl(fd, F_GETFL, 0);
 //  fcntl(0, F_SETFL, oldflags | O_NONBLOCK);
 }
 void term_cleanup() {
   if (tcsetattr(fd, 0, &old) == -1) perror("tcsetattr");
-  if (fcntl(0, F_SETFL, oldflags)) perror("fcntl");
+  if (fcntl(fd, F_SETFL, oldflags)) perror("fcntl");
 }
 int term_getkey() {
   char c = 0;
-  read(0, &c, 1);
+  read(fd, &c, 1);
   return c;
 }
